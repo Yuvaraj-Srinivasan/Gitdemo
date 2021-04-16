356cc8d9bf1d4f89411d57af691239da7b1ad4bc
Yuvaraj <yuvarajsrinivasan07@gmail.com>
2021-04-16 14:52:37 +0530

    Architect asian developement

--- src/test/java/QASeleniumtesting/Mavenjava/RestAPITest.java
+++ src/test/java/QASeleniumtesting/Mavenjava/RestAPITest.java
@@ -10,6 +10,10 @@ public class RestAPITest {
 		System.out.println("RestAPI 2");
 		System.out.println("RestAPI 3");
 		System.out.println("RestAPI 4");
+		System.out.println("RestAPI 5");
+		System.out.println("RestAPI 6");
+		System.out.println("RestAPI 7");
+		System.out.println("RestAPI 8");
 	}
 	@Test
 	public void RestAPI_logged() {
